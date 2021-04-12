531fcdd0b34f4130fc2055eabc16358681773aee
Pi <pchandra@uccs.edu>
2021-04-12 19:10:42 +0000

    adding with debug and CA verify

--- certs/Singleclient.py
+++ certs/Singleclient.py
@@ -7,13 +7,15 @@ def getIP():
 	return s.getsockname()[0]
 
 lsquic_dir = os.path.expanduser('~/oqs/lsquic')
+cert = os.path.expanduser('~/oqs/lsquic/certs/new_certs')
+
 
 client_ip = getIP()
 print(f'Client IP: {client_ip}')
 serverIP = input('Please enter the server IP: ')
 
-myCmd = f'{lsquic_dir}/build/bin/./http_client -0 0rttTest.txt -H www.example.com -s {serverIP}:4433 -p /'
-
+#myCmd = f'{lsquic_dir}/build/bin/./http_client -H www.example.com -s {serverIP}:4433 -p /'
+myCmd = f'{lsquic_dir}/build/bin/./http_client -C {cert}/dilithium2/key_CA.pem -H www.example.com -s {serverIP}:4433 -p /'
 n = 1000
 
 while n>0:

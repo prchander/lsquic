f7035754e7f534ba08994a5d8a0b7a9d1fdb48bd
Pi <pchandra@uccs.edu>
2021-04-12 18:55:02 +0000

    adding new dilithium2 certs

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/pi/oqs/lsquic
+# Build directory: /home/pi/oqs/lsquic/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("bin")
+subdirs("src")
+subdirs("tests")

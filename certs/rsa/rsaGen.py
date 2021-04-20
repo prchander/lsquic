54f7fbc16e49f7a772ae2c335b0a3a420d51cc74
Pi <pchandra@uccs.edu>
2021-04-20 17:23:33 +0000

    adding rainbow and hybrids

--- certs/rsa/rsaGen.py
+++ certs/rsa/rsaGen.py
@@ -1,13 +1,13 @@
 import os
 openssl_dir = os.path.expanduser('~/openssl')
 
-myCmd = f'{openssl_dir}/apps/openssl req -x509 -new -newkey rsa:3072 -keyout /home/pi/openssl/lsquic/rsa/key_CA.key -out /home/pi/openssl/lsquic/rsa/key_CA.pem -pkeyopt rsa_keygen_bits:3072 -nodes -subj "/CN=oqstest CA" -days 365 -config {openssl_dir}/apps/openssl.cnf'
+myCmd = f'{openssl_dir}/apps/openssl req -x509 -new -newkey rsa -keyout key_CA.key -out key_CA.pem -pkeyopt rsa_keygen_bits:4096 -nodes -subj "/CN=oqstest CA" -days 365 -config {openssl_dir}/apps/openssl.cnf > /dev/null 2>&1 '
 os.system(myCmd)
-myCmd = f'{openssl_dir}/apps/openssl genpkey -algorithm rsa -out /home/pi/openssl/lsquic/rsa/key_srv.pem -pkeyopt rsa_keygen_bits:3072'
+myCmd = f'{openssl_dir}/apps/openssl genpkey -algorithm rsa -out {openssl_dir}/CA-project/csr/key_srv.key -pkeyopt rsa_keygen_bits:4096 > /dev/null 2>&1'
 os.system(myCmd)
-myCmd = f'{openssl_dir}/apps/openssl req -new -key /home/pi/openssl/lsquic/rsa/key_srv.pem -out /home/pi/openssl/lsquic/rsa/key_srv.csr -nodes -pkeyopt rsa_keygen_bits:3072 -subj \'/CN=oqstest server\' -config {openssl_dir}/apps/openssl.cnf'
+myCmd = f'{openssl_dir}/apps/openssl req -new -key {openssl_dir}/CA-project/csr/key_srv.key -out {openssl_dir}/CA-project/csr/key_srv.csr -nodes -pkeyopt rsa_keygen_bits:4096 -subj \'/CN=oqstest server\' -config {openssl_dir}/apps/openssl.cnf > /dev/null 2>&1'
 os.system(myCmd)
-myCmd = f'{openssl_dir}/apps/openssl x509 -req -in /home/pi/openssl/lsquic/rsa/key_srv.csr -out /home/pi/openssl/lsquic/rsa/key_crt.pem -CA /home/pi/openssl/lsquic/rsa/key_CA.pem -CAkey /home/pi/openssl/lsquic/rsa/key_CA.key -CAcreateserial -days 365'
+myCmd = f'{openssl_dir}/apps/openssl x509 -req -in {openssl_dir}/CA-project/csr/key_srv.csr -out {openssl_dir}/CA-project/crt/key_crt.pem -CA key_CA.pem -CAkey key_CA.key -CAcreateserial -days 365' 
 os.system(myCmd)
 
 

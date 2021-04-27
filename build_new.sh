daf8d8f33d22bb4fd0cc754dd421258d7ffe7b3a
SimeoW <swuthier@uccs.edu>
2021-04-27 14:28:05 -0600

    Add build_new.sh

---
+++ build_new.sh
@@ -0,0 +1,45 @@
+cd ~/oqs
+BORINGSSL_DIR=$PWD/boringssl
+
+echo -n "Build LIBOQS? (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+	cd ~/oqs/liboqs
+	rm -rf build
+	mkdir build && cd build
+	cmake -GNinja -DCMAKE_INSTALL_PREFIX=$BORINGSSL_DIR/oqs -DOQS_USE_OPENSSL=OFF ..
+	ninja
+	sudo ninja install
+	echo "LIBOQS SUCCESSFULLY BUILT"
+fi
+
+
+
+echo -n "Build BORINGSSL? (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+	cd $BORINGSSL_DIR
+	rm -rf build
+	mkdir build && cd build
+	cmake -DBUILD_SHARED_LIBS=ON -GNinja ..
+	ninja
+	echo "BORINGSSL SUCCESSFULLY BUILT"
+fi
+
+
+
+echo -n "Build LSQUIC? (y/n)? "
+read answer
+if [ "$answer" != "${answer#[Yy]}" ] ;then
+	cd ~/oqs/lsquic
+	rm -rf build
+	mkdir build && cd build
+	cmake -DBORINGSSL_DIR=$BORINGSSL_DIR -DBORINGSSL_LIB_crypto=$BORINGSSL_DIR/build/crypto/libcrypto.so -DBORINGSSL_LIB_ssl=$BORINGSSL_DIR/build/ssl/libssl.so -GNinja ..
+	ninja
+	echo "lsquic SUCCESSFULLY BUILT"
+fi
+
+
+
+
+echo "Library building has finished."

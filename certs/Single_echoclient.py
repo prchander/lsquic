11298d505c8ddda355fc07732950ccda72e9ab24
SimeoW <swuthier@uccs.edu>
2021-05-29 10:05:52 -0600

    Add Single_echoclient.py

---
+++ certs/Single_echoclient.py
@@ -0,0 +1,25 @@
+import os
+import socket
+
+def getIP():
+	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	s.connect(('8.8.8.8', 1))
+	return s.getsockname()[0]
+
+lsquic_dir = os.path.expanduser('~/oqs/lsquic')
+#cert_dir = os.path.expanduser('~/oqs/lsquic/certs/rsa')
+
+#print(f'Certificate Directory: {cert_dir}')
+n = 1
+
+client_ip = getIP()
+print(f'Client IP: {client_ip}')
+print(f'Number of samples: ', n)
+serverIP = input('Please enter the server IP: ')
+
+#myCmd = f'{lsquic_dir}/build/bin/./echo_client -C {cert_dir} -H www.example.com -s {serverIP}:4433 -p /'
+myCmd = f'{lsquic_dir}/build/bin/./echo_client -H www.example.com -s {serverIP}:4433'
+
+while n>0:
+    os.system(myCmd)
+    n -= 1
